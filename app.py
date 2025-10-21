f56e01b873365c5925c6531223b6de8e5328c61a
Vinayak-Seth <vinayakseth2005@gmail.com>
2025-10-21 20:06:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ with tab3:
     h2h = matches[
         (((matches["team1"] == team) & (matches["team2"] == opponent_h2h)) |
          ((matches["team1"] == opponent_h2h) & (matches["team2"] == team))) &
-        (matches["season"] == season)
+        (matches["Season"] == season)
     ]
 
     st.write(f"Total Matches Played: {h2h.shape[0]}")
